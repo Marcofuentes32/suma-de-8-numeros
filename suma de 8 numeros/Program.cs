e562bc202549b862322a6428d3bdb0b7ae703dec
mollo <mollo@PCMOIKAS>
2023-10-11 08:47:07 -0600

    Agregar archivos de proyecto.

---
+++ suma de 8 numeros/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace suma_de_8_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
